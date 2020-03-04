6ba8667e6609be987da3b6a46a988457f03cae07
炮炮兵吐泡泡 <58586977+zzr946@users.noreply.github.com>
2020-03-04 14:44:17 +0800

    Update Constant.java
    修改key/code

--- SweetLove/src/com/softeem/tools/Constant.java
+++ SweetLove/src/com/softeem/tools/Constant.java
@@ -7,10 +7,10 @@ public class Constant {
 	public static final String REGIONID = "cn-hangzhou";
 	
 	// 短信接口AccessKeyID
-	public static final String ACCESSKEYID = "KTAIaqRbVXdVyCbH";//这里只是举个例子，该ID不能使用
+	public static final String ACCESSKEYID = "XXXKTAIaqXRbVXdVyXCbHXXX";//这里只是举个例子，该ID不能使用
 	
 	// 短信接口AccessKeySecret
-	public static final String ACCESSKEYSECRET = "N87c4iBG93gwSuvYNx8AVhxT82AlYL";//这里只是举个例子，该pwd不能使用
+	public static final String ACCESSKEYSECRET = "XXXN87c4iBGXX93gwSuvYNXXx8AVhxT8X2AlYLXXX";//这里只是举个例子，该pwd不能使用
 	
 	// 短信签名
 	public static final String SIGNNAME = "SweetLove";//此处以自己注册的签名为准
